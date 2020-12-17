6849f96e399e6037fe77f9765c2cc45da0d1c054
blaztan <periksoon191@gmail.com>
2020-12-17 15:29:00 +0100

    add PWA support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Perfume`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Landing page project made with gatsbyjs`,
     author: `zino chan`,
   },
   plugins: [
@@ -25,19 +25,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
-      },
-    },
-    // `gatsby-plugin-emotion`,
     `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-purgecss`,
@@ -50,18 +37,30 @@ module.exports = {
     {
       resolve: `gatsby-plugin-scroll-reveal`,
       options: {
-        threshold: 1, // Percentage of an element's area that needs to be visible to launch animation
-        once: true, // Defines if animation needs to be launched once
-        disable: false, // Flag for disabling animations
-
-        // Advanced Options
-        selector: "[data-sal]", // Selector of the elements to be animated
-        animateClassName: "sal-animate", // Class name which triggers animation
-        disabledClassName: "sal-disabled", // Class name which defines the disabled state
-        rootMargin: "0% 50%", // Corresponds to root's bounding box margin
-        enterEventName: "sal:in", // Enter event name
-        exitEventName: "sal:out", // Exit event name
+        threshold: 1,
+        once: true,
+        disable: false,
+        selector: "[data-sal]",
+        animateClassName: "sal-animate",
+        disabledClassName: "sal-disabled",
+        rootMargin: "0% 50%",
+        enterEventName: "sal:in",
+        exitEventName: "sal:out",
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Perfume Shop`,
+        short_name: `Perfume`,
+        description: `Gatsby Perfume landing page`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }
